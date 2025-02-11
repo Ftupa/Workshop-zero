f5e42eb29ad45bdefff2d83fbab24bf8278750fc
Ftupa <ftup149@aucklanduni.ac.nz>
2025-02-11 17:14:51 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200);
+  
+  circle(100,200,150);
+  fill(50,100,150);
+  stroke(100)
+
+  
+  circle(300,50,100)
+  fill(200,250,100)
+  stroke(200);
+  
+  circle(150,50,100)
+  fill(250,250,50);
+ stroke(50);
+  
+  
+  circle(250,150,200);
+  fill( 50,200,100);
+  stroke(100);
+  
+  
+  circle(50,150,50);
+  stroke(50);
+  fill(200,60,100);
+  
+}
